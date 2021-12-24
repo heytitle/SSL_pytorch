afb3e713b42a16eec091c26d758420b5f69124cb
Pat Chormai <pat.chormai@gmail.com>
2021-12-24 20:47:09 +0100

    use bt loss from lightly

--- models/barlow_twins.py
+++ models/barlow_twins.py
@@ -5,42 +5,42 @@ import torch.nn as nn
 
 from .utils import MLP
 
+
 class BarlowTwins(nn.Module):
     """
     Code from https://github.com/facebookresearch/barlowtwins
     """
-    def __init__(self, backbone_net,
-                 projector_hidden: Union[int, tuple] = (8192,8192,8192),
-                 λ: float = 0.0051):
+
+    def __init__(
+        self,
+        backbone_net,
+        projector_hidden: Union[int, tuple] = (8192, 8192, 8192),
+        λ: float = 0.0051,
+    ):
         super().__init__()
-        
+
         self.lambd = λ
-        
+
         self.backbone_net = backbone_net
         self.repre_dim = self.backbone_net.fc.in_features
         backbone_net.fc = nn.Identity()
-        
-        self.projector = MLP(self.repre_dim, projector_hidden, bias = False)
 
-        # Normalization layer for representations z1 + z2
-        self.bn = nn.BatchNorm1d(projector_hidden[-1], affine=False)
-    
-    def _off_diagonal(self, x):
-        # return a flattened view of the off-diagonal elements of a square matrix
-        n, m = x.shape
-        assert n == m
-        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
-    
+        self.projector = MLP(self.repre_dim, projector_hidden, bias=False)
+
     def loss_fn(self, z1, z2, λ):
-        # empirical cross-correlation matrix
-        c = self.bn(z1).T @ self.bn(z2)
-        c.div_(z1.size(0))
-        
-        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-        off_diag = self._off_diagonal(c).pow_(2).sum()
-        
-        return on_diag + λ * off_diag
-    
+        # taken from https://github.com/lightly-ai/lightly/blob/0be5e3b7b4e0b748fe9f2c2e31d2cf6ed3c5f77c/lightly/loss/barlow_twins_loss.py#L42
+        z1_norm = (z1 - z1.mean(0)) / z1.std(0)  # NxD
+        z2_norm = (z2 - z2.mean(0)) / z2.std(0)  # NxD
+
+        n, d = z1.shape
+        c = torch.mm(z1_norm.T, z2_norm) / n
+
+        c_diff = (c - torch.eye(D, device=device)).pow(2)  # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        c_diff[~torch.eye(D, dtype=bool)] *= λ
+
+        return c_diff.sum()
+
     def forward(self, x1, x2):
         z1 = self.projector(self.backbone_net(x1))
         z2 = self.projector(self.backbone_net(x2))
