a79f53a0a1764e3d9cc5f0c23fe56c99ba72aca0
Pat Chormai <pat.chormai@gmail.com>
2021-12-24 23:06:57 +0100

    fix typo

--- models/barlow_twins.py
+++ models/barlow_twins.py
@@ -35,7 +35,7 @@ class BarlowTwins(nn.Module):
         n, d = z1.shape
         c = torch.mm(z1_norm.T, z2_norm) / n
 
-        c_diff = (c - torch.eye(d, device=device)).pow(2)  # DxD
+        c_diff = (c - torch.eye(d, device=z1.device)).pow(2)  # DxD
         # multiply off-diagonal elems of c_diff by lambda
         c_diff[~torch.eye(d, dtype=bool)] *= λ
 
